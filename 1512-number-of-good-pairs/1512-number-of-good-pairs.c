c848dd6d3d5a15a438c25ed07f51da0846f4297d
shady236 <71782873+shady236@users.noreply.github.com>
2022-05-11 14:49:07 +0200

    Time: 0 ms (100.00%), Space: 5.8 MB (85.77%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.c
@@ -0,0 +1,13 @@
+
+
+int numIdenticalPairs(int* nums, int numsSize)
+{
+    int cnt = 0;
+    
+    for(int i=0; i<numsSize-1; i++)
+        for(int j=i+1; j<numsSize; j++)
+            if(nums[i] == nums[j])
+                cnt++;
+    
+    return cnt;
+}
