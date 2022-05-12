b011fb543f68049603fba1b292a19954cc9876e2
shady236 <71782873+shady236@users.noreply.github.com>
2022-05-12 20:07:43 +0200

    Time: 2 ms (57.58%), Space: 5.5 MB (75.54%) - LeetHub

--- 231-power-of-two/231-power-of-two.c
+++ 231-power-of-two/231-power-of-two.c
@@ -2,14 +2,13 @@
 
 bool isPowerOfTwo(int n)
 {
-    int cnt = 0;
-    
     if(n < 0)     return 0;
     
-    for(int i=0; i<32 && cnt<=1; i++)
+    int onesCnt = 0;
+    for(int i=0; i<32 && onesCnt<=1; i++)
         if((n>>i) & 1)
-            cnt++;
+            onesCnt++;
     
-    if(cnt == 1)        return 1;
+    if(onesCnt == 1)        return 1;
     return 0;
 }
