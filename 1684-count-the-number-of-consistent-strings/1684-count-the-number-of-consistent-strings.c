83fa675095d560afc9356e269fcb8de54480135e
shady236 <71782873+shady236@users.noreply.github.com>
2022-05-14 14:09:53 +0200

    Time: 65 ms (62.64%), Space: 12.4 MB (30.77%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.c
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.c
@@ -2,14 +2,14 @@
 
 int countConsistentStrings(char * allowed, char ** words, int wordsSize)
 {
-    char isCharAllowed['z' + 1] = {0};
+    char isCharAllowed[26] = {0};
     for(int i=0; allowed[i]; i++)
-        isCharAllowed[allowed[i]] = 1;
+        isCharAllowed[allowed[i] - 'a'] = 1;
     
     int cnt = 0;
     for(int i=0; i<wordsSize; i++)
         for(int j=0; words[i][j]; j++)
-            if(!isCharAllowed[words[i][j]])
+            if(!isCharAllowed[words[i][j] - 'a'])
                 break;
             else if(words[i][j+1] == 0)
                 cnt++;
