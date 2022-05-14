c05ac3b696457fd060dd3b7df3eee1da462e968c
shady236 <71782873+shady236@users.noreply.github.com>
2022-05-14 14:36:02 +0200

    Time: 0 ms (100.00%), Space: 5.7 MB (19.64%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.c
+++ 509-fibonacci-number/509-fibonacci-number.c
@@ -3,5 +3,16 @@
 int fib(int n)
 {
     if(n <= 1)      return n;
-    return fib(n-1) + fib(n-2);
+    
+    int prevFib2 = 0;
+    int prevFib1 = 1;
+    int res = 1;
+    for(int i=2; i<n; i++)
+    {
+        prevFib2 = prevFib1;
+        prevFib1 = res;
+        res = prevFib1 + prevFib2;
+    }
+    
+    return res;
 }
